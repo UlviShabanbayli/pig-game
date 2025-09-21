3a33123d537f6574fefefb702da5641e621da90b
Ulvi <ulvi.shabanov@gmail.com>
2025-09-21 16:36:54 +0400

    bug fixxed

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ btnHold.addEventListener('click', () => {
       scores[activePlayer];
 
     // 2. check if player's score is >= 100
-    if (scores[activePlayer] >= 10) {
+    if (scores[activePlayer] >= 100) {
       // finish the game
       playing = false;
       document
